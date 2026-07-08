7416945ea86ab985d943af5fb4566c5a447cc19a
Alicja Cielecka <157830089+alicjac0@users.noreply.github.com>
2026-07-08 10:49:10 +0200

    ui update

--- main.js
+++ main.js
@@ -14,3 +14,16 @@ function bodyConfiguration(input){
         element.classList.add("unactive")
     }});
 }
+
+function values(){
+  planetmass = Number(document.getElementById('planet__mass').value);
+  starmass = Number(document.getElementById('star__mass').value);
+  blackholemass = Number(document.getElementById('blackhole__mass').value);
+  timespeed = Number(document.getElementById('time__speed').value);
+
+  document.getElementById('planet__value').innerText = planetmass + " CU";
+  document.getElementById('star__value').innerText = starmass + " CU";
+  document.getElementById('blackhole__value').innerText = blackholemass + " CU";
+  document.getElementById('time__value').innerText = timespeed + "x";
+
+}
